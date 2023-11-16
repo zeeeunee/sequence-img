6d176b19fa5537fcb9a63dc09de0b427d8647586
zeeeunee <wldmstv@gmail.com>
2023-11-16 16:44:17 +0900

    동적으로 생성된 이미지DOM을 인수받아서 소스이미지 로딩유무 체크함수

--- js/main.js
+++ js/main.js
@@ -13,34 +13,35 @@ const frame = document.querySelector('figure');
 const mask = document.querySelector('.mask');
 const countEl = mask.querySelector('span');
 const imgNum = 200;
-
 let count = 0;
 
-createImgs(frame, imgNum);
-const imgs = frame.querySelectorAll('img');
+const imgs = createImgs(frame, imgNum);
+imgLoadedCheck(imgs);
 
-imgs.forEach((img) => {
-	img.addEventListener('error', (e) => {
-		e.currentTarget.setAttribute('src', 'img/logo.png');
-	});
-
-	img.addEventListener('load', () => {
-		countEl.innerText = parseInt((count / imgNum) * 100) + 1;
-		count++;
-		if (count === imgNum) {
-			mask.remove();
-		}
-	frame.addEventListener('mousemove', (e) => {
+frame.addEventListener('mousemove', (e) => {
+	const { pageX } = e;
+	const percent = parseInt((pageX / window.innerWidth) * imgNum);
+	imgs.forEach((img) => (img.style.visibility = 'hidden'));
 	imgs[percent].style.visibility = 'visible';
 });
-  
-
-//동적 이미지 생성함수 분리
-function createImgs(frame, imgNum, imgName = 'pic'){
+//동적 이미지 생성함수 분리 (돔생성하자 마자 바로 리턴해서 활용가능하록 처리)
+function createImgs(frame, imgNum, imgName = 'pic') {
 	let tags = '';
-
-	for (let i = 0; i < imgNum; i++) {
-		tags += `<img src='img/${imgName}${i}.jpg' />`;
-	}
+	for (let i = 0; i < imgNum; i++) tags += `<img src='img/${imgName}${i}.jpg' />`;
 	frame.innerHTML = tags;
+	return frame.querySelectorAll('img');
+}
+
+//이미지소스 로딩유무 체크함수
+function imgLoadedCheck(imgs) {
+	imgs.forEach((img) => {
+		img.addEventListener('error', (e) => {
+			e.currentTarget.setAttribute('src', 'img/logo.png');
+		});
+		img.addEventListener('load', () => {
+			countEl.innerText = parseInt((count / imgNum) * 100) + 1;
+			count++;
+			if (count === imgNum) mask.remove();
+		});
+	});
 }
